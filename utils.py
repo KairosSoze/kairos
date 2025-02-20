3ac451ec42c68366e0b40733a29dab562b7a59d4
mevert87 <mevert87@gmail.com>
2025-02-20 14:29:45 -0600

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+def visualize_structure(structure):
+    """
+    Visualizes the 3D structure of the protein.
+
+    Args:
+      structure: The 3D structure of the protein.
+    """
+    # Implement visualization logic here
+    # Example: Use a library like PyMOL or matplotlib to visualize the structure
+    #...
