24172ce133152d5518c4818ec6c156602b1ab005
mevert87 <mevert87@gmail.com>
2025-02-22 14:41:01 -0600

    Update quantum_encoding.py

--- quantum_encoding.py
+++ quantum_encoding.py
@@ -14,17 +14,87 @@ class QuantumEncoding:
         self.qc = QuantumCircuit(self.n_qubits)
 
     def encode(self):
-        """
-        Encodes the amino acid sequence into the quantum circuit.
-        """
-        # Assign each amino acid to a specific qubit state
-        for i, amino_acid in enumerate(self.sequence):
-            if amino_acid == 'A':
-                # Encode Alanine as |0>
-                pass
-            elif amino_acid == 'C':
-                # Encode Cysteine as |1>
-                self.qc.x(i)
-            #... (encoding for other amino acids)
+    """
+    Encodes the amino acid sequence into the quantum circuit.
+    """
+    # Assign each amino acid to a specific qubit state
+    for i, amino_acid in enumerate(self.sequence):
+        if amino_acid == 'A':
+            # Encode Alanine as |0>
+            pass
+        elif amino_acid == 'C':
+            # Encode Cysteine as |1>
+            self.qc.x(i)
+        elif amino_acid == 'D':
+            # Encode Aspartic Acid as |+>
+            self.qc.h(i)
+        elif amino_acid == 'E':
+            # Encode Glutamic Acid as |->
+            self.qc.x(i)
+            self.qc.h(i)
+        elif amino_acid == 'F':
+            # Encode Phenylalanine as |i>
+            self.qc.h(i)
+            self.qc.s(i)
+        elif amino_acid == 'G':
+            # Encode Glycine as |-i>
+            self.qc.x(i)
+            self.qc.h(i)
+            self.qc.s(i)
+        elif amino_acid == 'H':
+            # Encode Histidine as |00>
+            pass  # No action needed for two qubits in state 0
+        elif amino_acid == 'I':
+            # Encode Isoleucine as |01>
+            self.qc.x(i + 1)
+        elif amino_acid == 'K':
+            # Encode Lysine as |10>
+            self.qc.x(i)
+        elif amino_acid == 'L':
+            # Encode Leucine as |11>
+            self.qc.x(i)
+            self.qc.x(i + 1)
+        elif amino_acid == 'M':
+            # Encode Methionine as |+0>
+            self.qc.h(i)
+        elif amino_acid == 'N':
+            # Encode Asparagine as |+1>
+            self.qc.h(i)
+            self.qc.x(i + 1)
+        elif amino_acid == 'P':
+            # Encode Proline as |-0>
+            self.qc.x(i)
+            self.qc.h(i)
+        elif amino_acid == 'Q':
+            # Encode Glutamine as |-1>
+            self.qc.x(i)
+            self.qc.h(i)
+            self.qc.x(i + 1)
+        elif amino_acid == 'R':
+            # Encode Arginine as |i0>
+            self.qc.h(i)
+            self.qc.s(i)
+        elif amino_acid == 'S':
+            # Encode Serine as |i1>
+            self.qc.h(i)
+            self.qc.s(i)
+            self.qc.x(i + 1)
+        elif amino_acid == 'T':
+            # Encode Threonine as |-i0>
+            self.qc.x(i)
+            self.qc.h(i)
+            self.qc.s(i)
+        elif amino_acid == 'V':
+            # Encode Valine as |-i1>
+            self.qc.x(i)
+            self.qc.h(i)
+            self.qc.s(i)
+            self.qc.x(i + 1)
+        elif amino_acid == 'W':
+            # Encode Tryptophan as |000>
+            pass  # No action needed for three qubits in state 0
+        elif amino_acid == 'Y':
+            # Encode Tyrosine as |001>
+            self.qc.x(i + 2)
 
-        return self.qc
+    return self.qc
