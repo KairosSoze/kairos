ecc90bbbdd1d2fce43f4b23f35a4884508e147ff
mevert87 <mevert87@gmail.com>
2025-02-22 14:45:03 -0600

    Update entaglement_layer.py

--- entaglement_layer.py
+++ entaglement_layer.py
@@ -12,16 +12,18 @@ class EntanglementLayer:
         self.qc = quantum_circuit
 
     def entangle(self):
-        """
-        Applies an entanglement layer to the quantum circuit.
-        """
-        n_qubits = self.qc.num_qubits
+    """
+    Applies an entanglement layer to the quantum circuit.
+    """
+    n_qubits = self.qc.num_qubits
 
-        # Apply controlled-NOT gates between adjacent qubits
-        for i in range(n_qubits - 1):
-            self.qc.cx(i, i + 1)
+    # Apply controlled-NOT gates between adjacent qubits
+    for i in range(n_qubits - 1):
+        self.qc.cx(i, i + 1)
 
-        # Apply additional entanglement operations as needed
-        #...
+    # Apply additional entanglement operations for non-adjacent qubits
+    for i in range(n_qubits - 2):
+        for j in range(i + 2, n_qubits):
+            self.qc.crx(np.pi / 3, i, j)  # Controlled-RX rotation
 
-        return self.qc
+    return self.qc
