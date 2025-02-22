3647cca440036b4899bcb7decf0c5283ae868e1a
mevert87 <mevert87@gmail.com>
2025-02-22 14:51:34 -0600

    Update energy_minimzation.py

--- energy_minimzation.py
+++ energy_minimzation.py
@@ -1,30 +1,73 @@
 from qiskit import Aer, execute
 from qiskit.aqua.algorithms import VQE
 from qiskit.aqua.components.optimizers import SPSA
+from qiskit.aqua.operators import WeightedPauliOperator
+from qiskit.chemistry.components.variational_forms import UCCSD
+from qiskit.chemistry.drivers import PySCFDriver, UnitsType
+from qiskit.chemistry.core import Hamiltonian, TransformationType, QubitMappingType
 
 class EnergyMinimization:
 
-    def __init__(self, quantum_circuit, energy_function):
+    def __init__(self, molecule, qubit_mapping=QubitMappingType.PARITY,
+                 two_qubit_reduction=True, freeze_core=True,
+                 transformation=TransformationType.FULL):
         """
-        Initializes the EnergyMinimization with a quantum circuit and an energy function.
+        Initializes the EnergyMinimization with a molecule and other parameters.
 
         Args:
-          quantum_circuit: The quantum circuit representing the protein.
-          energy_function: The function to calculate the energy of the protein.
+          molecule: The molecule to perform energy minimization on.
+          qubit_mapping: The qubit mapping type to use.
+          two_qubit_reduction: Whether to use two-qubit reduction.
+          freeze_core: Whether to freeze core orbitals.
+          transformation: The transformation type to use.
         """
-        self.qc = quantum_circuit
-        self.energy_function = energy_function
+        self.molecule = molecule
+        self.qubit_mapping = qubit_mapping
+        self.two_qubit_reduction = two_qubit_reduction
+        self.freeze_core = freeze_core
+        self.transformation = transformation
 
     def minimize(self):
         """
         Performs energy minimization using a variational quantum eigensolver (VQE).
 
         Returns:
-          The optimized quantum circuit representing the protein's ground state.
+          The ground state energy of the molecule.
         """
+        driver = PySCFDriver(
+            atom=self.molecule,
+            unit=UnitsType.ANGSTROM,
+            charge=0,
+            spin=0,
+            basis='sto3g'
+        )
+        qmolecule = driver.run()
+
+        core = Hamiltonian(
+            transformation=self.transformation,
+            qubit_mapping=self.qubit_mapping,
+            two_qubit_reduction=self.two_qubit_reduction,
+            freeze_core=self.freeze_core
+        )
+        qubit_op, _ = core.run(qmolecule)
+
+        # Set up the VQE algorithm
         backend = Aer.get_backend('statevector_simulator')
         optimizer = SPSA(maxiter=100)
-        vqe = VQE(self.qc, self.energy_function, optimizer)
+        var_form = UCCSD(
+            num_orbitals=core.molecule_info['num_orbitals'],
+            num_particles=core.molecule_info['num_particles'],
+            active_occupied=None,
+            active_unoccupied=None,
+            initial_state=None,
+            qubit_mapping=self.qubit_mapping,
+            two_qubit_reduction=self.two_qubit_reduction,
+            num_time_slices=1
+        )
+        vqe = VQE(qubit_op, var_form, optimizer)
+
+        # Run the VQE algorithm
         result = vqe.run(backend)
 
-        return result['eigenstate']
+        # Return the ground state energy
+        return result['eigenvalue'].real
